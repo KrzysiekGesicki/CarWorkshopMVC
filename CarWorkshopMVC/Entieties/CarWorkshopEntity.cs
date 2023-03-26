78bd1d7b1c8530ec6723c2a67f9454b73e9bb22b
Krzysiek Gesicki <krzysiek.gesicki@gmail.com>
2023-03-26 18:29:50 +0200

    added CarWorkshopEntity class

---
+++ CarWorkshopMVC/Entieties/CarWorkshopEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CarWorkshopMVC.Entieties
+{
+    public class CarWorkshopEntity
+    {
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public string Address { get; set; } = default!;
+        public string? PhoneNumber { get; set; }
+    }
+}
